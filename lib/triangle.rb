650d89b7c5236be634dc6d59a53aa2e83c8d7b9a
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-02-20 22:19:34 +0300

    complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  attr_reader :sides
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3].sort
+    raise TriangleError if @sides.any? { |s| s <= 0 }
+    raise TriangleError unless @sides[0] + @sides[1] > @sides[2]
+  end
+
+  def kind
+    case @sides.uniq.size
+    when 1 then :equilateral
+    when 2 then :isosceles
+    else        :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
